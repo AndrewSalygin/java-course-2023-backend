59d37cf898a0504ac88fe24e76ec4233141f2bca
AndrewSalygin <andrew.salygin@gmail.com>
2024-03-17 19:41:42 +0400

    Start do hw5-bonus

--- scrapper/src/main/java/edu/java/client/bot/dto/request/LinkUpdate.java
+++ scrapper/src/main/java/edu/java/client/bot/dto/request/LinkUpdate.java
@@ -3,12 +3,23 @@ package edu.java.client.bot.dto.request;
 import jakarta.validation.constraints.NotEmpty;
 import java.net.URL;
 import java.util.List;
+import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 
 public record LinkUpdate(
+
     @NotNull Long id,
+<<<<<<< HEAD
     @NotNull URL url,
+=======
+
+    @NotNull URL link,
+
+>>>>>>> 5498748 (Start do hw5-bonus)
     @NotEmpty String description,
-    @NotEmpty List<Long> tgChatIds
+
+    @NotEmpty List<Long> tgChatIds,
+
+    @NotEmpty Map<String, String> metaInfo
 ) {
 }
